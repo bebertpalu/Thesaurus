e48ab153313ffb98d887fce8feac26302229a408
Bob Bernard <cyril.monmouton@etud.univ-montp2.fr>
2012-12-08 01:07:34 +0100

    finition exemple fonctionnel

---
+++ MVC/global/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Identifiants pour la base de données. Nécessaires a PDO2.
+define('SQL_DSN',      'mysql:dbname=tutoriel;host=localhost');
+define('SQL_USERNAME', 'root');
+define('SQL_PASSWORD', '');
+?>
