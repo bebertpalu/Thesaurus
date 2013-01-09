1790e3955400b82a1aef5e6106f18fa997cb0872
Bob Bernard <cyril.monmouton@etud.univ-montp2.fr>
2013-01-09 18:44:07 +0100

    creation controleur listePermutee + vue associee + vue d'erreur

--- src/controleurs/listepermutee.controleur.php
+++ src/controleurs/listepermutee.controleur.php
@@ -1,6 +1,32 @@
 <?php
 /**
-  * @author Abdelhamid Belarbi
+  * @author Bob !
   * Permet l'affichage du thésaurus par liste permutée.
-  */
+ **/
+
+require_once 'controleurs/libs/Smarty.class.php';
+require_once 'modeles/descripteur.class.php';
+
+$template = new Smarty();
+$resultats = array();
+
+if(isset($_POST['keyword']) && !empty($_POST['keyword']))
+{
+	/* 	
+		La fonction Descripteur::rechercher() actuelle est idéale pour la recherche par liste permutée 
+		mais pour plus de compréhension je l'ai renommé. 
+	*/
+
+	// $resultats = Descripteur::rechercheListePermutee($_POST['keyword']);
+	$template->assign('resultats', $resultats);
+	$template->display('vues/resultat.vue.html');
+}
+else 
+{
+	// Pour déférencier une recherche non fructueuse et une erreur de saisie
+	$template->display('vues/erreur.vue.html');
+}
+
+
+
 ?>
