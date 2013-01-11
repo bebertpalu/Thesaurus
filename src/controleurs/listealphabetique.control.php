334c4b46123dc038c3724ae2db23fcf1c2b7d4cc
denguiri <denguiri.merouane@gmail.com>
2013-01-11 12:34:46 +0100

    Update src/controleurs/listealphabetique.control.php

--- src/controleurs/listealphabetique.control.php
+++ src/controleurs/listealphabetique.control.php
@@ -5,6 +5,21 @@
   */
 
 require_once 'controleurs/libs/Smarty.class.php';
+require_once 'modeles/descripteur.class.php';
 
+$template = new Smarty();
+
+if (!empty($_GET['libelle']))
+{
+
+}
+else
+{
+	$resultats = array();
+}
+
+
+$template->assign('resultats', $resultats);
+$template->display('vues/resultat.vue.html');
 
 ?>
