441d33c36f1b297d03ba38743754b803f9064993
abelarbi <abelarbi@p7.etu.info-ufr.univ-montp2.fr>
2013-01-14 13:43:56 +0100

    Smarty fonctionne bien + début remplissage BD

---
+++ src/controleurs/listealphabetique.controleur.php
@@ -0,0 +1,25 @@
+<?php
+/**
+  * @author Abdelhamid Belarbi
+  * Permet l'affichage du thésaurus par liste alphabétique.
+  */
+
+require_once 'controleurs/libs/Smarty.class.php';
+require_once 'modeles/descripteur.class.php';
+
+$template = new Smarty();
+
+if (!empty($_GET['libelle']))
+{
+
+}
+else
+{
+	$resultats = array();
+}
+
+
+$template->assign('resultats', $resultats);
+$template->display('vues/resultat.vue.html');
+
+?>
---
+++ src/controleurs/recherche.controleur.php
@@ -0,0 +1,28 @@
+<?php
+/**
+  * @author Abdelhamid Belarbi
+  * Effectue la recherche du libellé passé dans la variable $_POST.
+  */
+
+require_once 'controleurs/libs/Smarty.class.php';
+require_once 'modeles/descripteur.class.php';
+
+// Pour éviter un bug de strfmtime() utilisée par Smarty.
+ini_set('date.timezone', 'Europe/Paris');
+
+$template = new Smarty();
+
+if (!empty($_GET['libelle']))
+{
+	$resultats = Descripteur::rechercher($_POST['libelle']);
+}
+else
+{
+	$resultats = array();
+}
+
+
+$template->assign('resultats', $resultats);
+$template->assign('libelle', $_POST['libelle']);
+$template->display('vues/resultat.vue.html');
+?>
